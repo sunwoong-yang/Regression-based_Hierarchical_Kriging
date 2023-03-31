d7ec8416f74bd7b5358778891db3a6946629ce1d
Woong <sunwoongy@gmail.com>
2023-03-31 23:32:05 +0900

    Add MFDNN

--- test.py
+++ test.py
@@ -28,13 +28,11 @@ class ToyProblem(torch.utils.data.Dataset):
         return self.x[idx], self.y[idx]
 
 
-# Instantiate the toy problem dataset
-# dataset = ToyProblem(5)
-# for x, y in dataset:
-#     print(x.item(),y.item())
-N_inp, N_out = 1, 2
-# header, dataloader = csv2Dat(N_inp=N_inp, dir="ToyProblem3.csv")
-inp_header, out_header, train_x, train_y = csv2Num(N_inp=N_inp, dir="ToyProblem3.csv")
+
+# N_inp, N_out = 1, 2
+# inp_header, out_header, train_x, train_y = csv2Num(N_inp=N_inp, dir="ToyProblem3.csv")
+N_inp, N_out = 1, 1
+inp_header, out_header, train_x, train_y = csv2Num(N_inp=N_inp, dir="HF2.csv")
 LF_inp_header, LF_out_header, LF_train_x, LF_train_y = csv2Num(N_inp=N_inp, dir="LF2.csv")
 HF_inp_header, HF_out_header, HF_train_x, HF_train_y = csv2Num(N_inp=N_inp, dir="HF2.csv")
 # header2, dataloader2 = csv2Dat(N_inp=1, dir="ToyProblem3.csv")
@@ -87,8 +85,8 @@ gprs.fit(train_x, train_y)
 
 mfdnn = MFDNN(input_dim=1, output_dim=1)
 criterion_ = nn.MSELoss()
-mfdnn.add_fidelity(hidden_layers=[20,20], activation="GELU", criterion=criterion_, lr=1e-3, epochs=1000)
-mfdnn.add_fidelity(hidden_layers=[20,20,20], activation="GELU", criterion=criterion_, lr=1e-3, epochs=1000)
+mfdnn.add_fidelity(hidden_layers=[20,20], activation="Tanh", criterion=criterion_, lr=1e-3, epochs=1000)
+mfdnn.add_fidelity(hidden_layers=[10,10], activation="Tanh", criterion=criterion_, lr=1e-3, epochs=1000)
 
 mfdnn.fit(train_x=[LF_train_x, HF_train_x], train_y=[LF_train_y, HF_train_y])
 
@@ -128,15 +126,15 @@ plt.legend()
 plt.title("Y1")
 plt.show()
 
-plt.scatter(gprs.models[1].train_x, gprs.models[1].train_y, c='k', label=f'Train data {out_header[1]}')
-plt.plot(X_test, Y_gpr[:,1], c='r', label=f"GPR {out_header[1]}")
-plt.plot(X_test, Y_mlp[:,1], c='g', label=f"MLP {out_header[1]}")
-plt.plot(X_test, Y_de[:,1], c='b', label=f"DE {out_header[1]}")
-plt.fill_between(X_test.flatten(), Y_gpr[:,1]-30*STD_gpr[:,1], Y_gpr[:,1]+30*STD_gpr[:,1], color='r', alpha=.5)
-plt.fill_between(X_test.flatten(), Y_de[:,1]-30*epis_std[:,1], Y_de[:,1]+30*epis_std[:,1], color='b', alpha=.5)
-plt.legend()
-plt.title("Y2")
-plt.show()
+# plt.scatter(gprs.models[1].train_x, gprs.models[1].train_y, c='k', label=f'Train data {out_header[1]}')
+# plt.plot(X_test, Y_gpr[:,1], c='r', label=f"GPR {out_header[1]}")
+# plt.plot(X_test, Y_mlp[:,1], c='g', label=f"MLP {out_header[1]}")
+# plt.plot(X_test, Y_de[:,1], c='b', label=f"DE {out_header[1]}")
+# plt.fill_between(X_test.flatten(), Y_gpr[:,1]-30*STD_gpr[:,1], Y_gpr[:,1]+30*STD_gpr[:,1], color='r', alpha=.5)
+# plt.fill_between(X_test.flatten(), Y_de[:,1]-30*epis_std[:,1], Y_de[:,1]+30*epis_std[:,1], color='b', alpha=.5)
+# plt.legend()
+# plt.title("Y2")
+# plt.show()
 
 def HF(x):
     return (6 * x - 2) ** 2 * np.sin(12 * x - 4)
