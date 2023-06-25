0bbb2ec02e795c3350cb8de215c37f75ffc0f0ce
Woong <sunwoongy@gmail.com>
2023-06-25 16:44:12 +0900

    first commit

--- examples/Test_function_7.py
+++ examples/Test_function_7.py
@@ -7,31 +7,39 @@ import matplotlib.pyplot as plt
 from pyDOE import lhs
 
 """
-Test_function_6 can be found here
+Test_function_7 can be found here
 Xiong, F., Ren, C., Mo, B., Li, C., & Hu, X. (2023). A new adaptive multi-fidelity metamodel method using meta-learning and Bayesian deep learning. Structural and Multidisciplinary Optimization, 66(3), 58.
 [Link] https://link.springer.com/article/10.1007/s00158-023-03518-8
+However, LF and MF functions in the above ref are erroneous. Therefore, in this code, sigma from i=1 to 15 is changed to "i=1 ~ 14"
 """
-in_dim = 10
+in_dim = 15
 out_dim = 1
 def LF_function(x):
-    x1, x2, x3, x4, x5, x6, x7, x8, x9, x10 = x[:, 0], x[:, 1], x[:, 2], x[:, 3], x[:, 4], x[:, 5], x[:, 6], x[:, 7], x[:, 8], x[:, 9]
-    return 0.5 * x1**2 + 0.6 * x2**2 + 0.3 * x1*x2 - 3 * x1 - 5 * x2 + (x3 - 2)**2 + 4.5 * (x4 - 5)**2 + 1.2 * (x5 - 3)**2 + \
-        2 * (x6-1)**2 + 3 * x7**2 + 7 * (x8-3)**2 + 2 * (x9-2)**2 + (x10 - 1)**2 + 10
+    y = 0.8 * (x[:, 0] - 1)**2
+    for i in range(1, in_dim):
+        y += 0.7 * (i+1) * (2 * x[:, i]**2 - x[:, i-1])**2
+    for i in range(0, in_dim - 1):
+        y += -0.2 * x[:, i] * x[:, i+1]
+    return y
 
 def MF_function(x):
-    x1, x2, x3, x4, x5, x6, x7, x8, x9, x10 = x[:, 0], x[:, 1], x[:, 2], x[:, 3], x[:, 4], x[:, 5], x[:, 6], x[:, 7], x[:, 8], x[:, 9]
-    return 0.8 * x1**2 + 0.7 * x2**2 + 0.5 * x1*x2 - 4 * x1 - 6 * x2 + (x3 - 2)**2 + 4 * (x4 - 5)**2 + 1.1 * (x5 - 3)**2 + \
-        2 * (x6-1)**2 + 4.5 * x7**2 + 7 * (x8-3)**2 + 2 * (x9-2)**2 + (x10 - 1)**2 + 10
+    y = 0.9 * (x[:, 0] - 1) ** 2
+    for i in range(1, in_dim):
+        y += 0.9 * (i + 1) * (2 * x[:, i]**2 - x[:, i - 1]) ** 2
+    for i in range(0, in_dim - 1):
+        y += -0.1 * x[:, i] * x[:, i + 1]
+    return y
 
 def HF_function(x):
-    x1, x2, x3, x4, x5, x6, x7, x8, x9, x10 = x[:, 0], x[:, 1], x[:, 2], x[:, 3], x[:, 4], x[:, 5], x[:, 6], x[:, 7], x[:, 8], x[:, 9]
-    return 1 * x1**2 + 1 * x2**2 + 1 * x1*x2 - 4 * x1 - 6 * x2 + (x3 - 2)**2 + 4 * (x4 - 5)**2 + 1 * (x5 - 3)**2 + \
-        2 * (x6-1)**2 + 5 * x7**2 + 7 * (x8-3)**2 + 2 * (x9-2)**2 + (x10 - 1)**2 + 11
+    y = 1.0 * (x[:, 0] - 1) ** 2
+    for i in range(1, in_dim):
+        y += 1.0 * (i + 1) * (2 * x[:, i]**2 - x[:, i - 1]) ** 2
+    return y
 
 
-LF_x = lhs(in_dim, samples=100, criterion='maximin') * 1 + 2
-MF_x = lhs(in_dim, samples=50, criterion='maximin') * 1 + 2
-HF_x = lhs(in_dim, samples=10, criterion='maximin') * 1 + 2
+LF_x = lhs(in_dim, samples=200, criterion='maximin') * 1 + 0.5
+MF_x = lhs(in_dim, samples=100, criterion='maximin') * 1 + 0.5
+HF_x = lhs(in_dim, samples=50, criterion='maximin') * 1 + 0.5
 
 LF_y = LF_function(LF_x).reshape(-1,1)
 MF_y = MF_function(MF_x).reshape(-1,1)
@@ -53,7 +61,7 @@ hfdnn.add_fidelity(hidden_layers=[10, 10], activation="GELU", criterion=criterio
 
 hfdnn.fit(train_x=[HF_x], train_y=[HF_y])
 
-test_x = lhs(in_dim, samples=100, criterion='maximin') * 1 + 2
+test_x = lhs(in_dim, samples=100, criterion='maximin') * 1 + 0.5
 
 pred_LF_y = mfdnn.predict(test_x, pred_fidelity=0)
 pred_MF_y = mfdnn.predict(test_x, pred_fidelity=1)
